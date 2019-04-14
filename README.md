08bfbc2c91cd415314115c5e775aafa70686ed94
Ryan Dancy <ryan@keal.ca>
2019-04-14 13:45:10 -0400

    Add XmlPersistor, PersistenceUtil
    XmlPersistor<R> is a class which is tasked with persisting and
    regenerating objects of type R to and from XML. Its constructor takes
    a Class<R> and verifies it. It has two methods: toXml(R root) takes an
    object of type R and persists it, returning a Document, while
    fromXml(Document doc) regenerates an object of type R from a Document.
    Neither are implemented as of yet.
    
    PersistenceUtil is a package-level utility class which contains utility
    methods for dealing with persistence. Currently, it contains two
    methods: verifyAndGetPersistable(Class<?> cls) verifies the existence
    and correctness of the @Persistable annotation on cls, then returns it,
    while isValidXmlTag(String tag) returns whether its parameter is a valid
    XML tag.
    
    As well, update the documentation in README.md and PersistenceException
    to reflect that PersistenceException will be the sole custom exception
    and that XmlPersistor's constructor has arguments.

--- README.md
+++ README.md
@@ -66,37 +66,31 @@ Marks a (non-static) field that it is to be persisted. `value` gives the name of
 the XML element representing this field. Only primitives, `String`s, and objects marked
 `@Persistable` may be `@Persist`ed. (Support for collection types will be added later.)
 
-### `XmlPersistor`
+### `XmlPersistor<R>`
 
 A utility class containing methods which convert XML to Java objects and vice versa. Further
-methods dealing with files may be added later.
+methods dealing with files may be added later. `R` is the type of the root objects which may
+be persisted.
 
-#### `XmlPersistor()`
+#### `XmlPersistor(Class<R> rootClass)`
 
-Instantiate an `XmlPersistor`. Using an instance rather than static methods is best
-in case state is stored in the future.
+Instantiate an `XmlPersistor`. `rootClass` is the class of the root objects which may be
+persisted by this specific `XmlPersistor`. It must be `@Persistable(toplevel=true)`; if
+not, `PersistenceException` is thrown.
 
-#### `org.w3c.dom.Document toXml(Object root) throws PersistenceException`
+#### `org.w3c.dom.Document toXml(R root)`
 
 Persists an object, `root`, into an XML DOM `Document` which may be written to a file for
 persistence. `root` must be `@Persistable(toplevel=true)`. `PersistenceException`, a
 runtime exception, will be thrown if there are errors such as `@Persist`ing a
 non-`@Persistable` class.
 
-#### `<T> T fromXml(org.w3c.dom.Document doc, Class<T> cls) throws RegenerationException`
+#### `R fromXml(org.w3c.dom.Document doc)`
 
-Retrieves an `@Persistable(toplevel=true)` object of type `T` from the XML DOM document
-`doc`. The object's class is `cls`. `RegenerationException` is thrown if there are errors
-in the XML document or if there are mismatches such as the `root="true"` element of the
-document not being of type `T`.
+Retrieves an `@Persistable(toplevel=true)` object of type `R` from the XML DOM document
+`doc`. `PersistenceException` is thrown if there are errors in the XML document or if there
+are mismatches such as the `root="true"` element of the document not being of type `R`.
 
 ### `PersistenceException`
 
-A subclass of `RuntimeException`, this is thrown if there are errors in persisting the
-class structure.
-
-### `RegenerationException`
-
-A checked exception, this is thrown if there are errors in regenerating the class
-structure from the XML.
- 
+A runtime exception, this is thrown if there are errors in persisting the class structure.
---
+++ src/main/java/ca/keal/persistence/PersistenceUtil.java
@@ -0,0 +1,76 @@
+package ca.keal.persistence;
+
+/**
+ * Contains utility methods for use by the persistence logic.
+ */
+final class PersistenceUtil {
+  
+  /** Matches valid XML tags - https://stackoverflow.com/a/5396246 */
+  private static final String XML_TAG_REGEX = "^[:A-Z_a-z\\u00C0\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02ff\\u0370-\\u037d"
+      + "\\u037f-\\u1fff\\u200c\\u200d\\u2070-\\u218f\\u2c00-\\u2fef\\u3001-\\ud7ff\\uf900-\\ufdcf\\ufdf0-\\ufffd" 
+      + "\\u10000-\\uEFFFF][:A-Z_a-z\\u00C0\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02ff\\u0370-\\u037d\\u037f-\\u1fff\\u200c"
+      + "\\u200d\\u2070-\\u218f\\u2c00-\\u2fef\\u3001-\\udfff\\uf900-\\ufdcf\\ufdf0-\\ufffd\\-.0-9\\u00b7\\u0300-" 
+      + "\\u036f\\u203f-\\u2040]*\\Z";
+  
+  /** This class cannot be instantiated */
+  private PersistenceUtil() {}
+  
+  /**
+   * Verify that {@code cls} is annotated with @{@link Persistable} properly, then return the annotation.
+   * @param cls The class whose @{@link Persistable} annotation will be verified and returned.
+   * @return The @{@link Persistable} annotation of {@code cls}.
+   * @throws PersistenceException If the class is not annotated with @{@link Persistable} or if its
+   *  {@link Persistable#toplevel} parameter is {@code true} and any of the following conditions is not met:
+   *  <ul>
+   *    <li>{@code tag} is not supplied</li>
+   *    <li>{@code tag} is not a valid XML tag, as determined by {@link #isValidXmlTag(String)}</li>
+   *    <li>{@code idField} is not supplied</li>
+   *    <li>{@code idField} is not a valid instance-level field in {@code cls}</li>
+   *  </ul>
+   */
+  static Persistable verifyAndGetPersistable(Class<?> cls) {
+    Persistable persistable = cls.getAnnotation(Persistable.class);
+    
+    // Verify that it's @Persistable
+    if (persistable == null) {
+      throw new PersistenceException(cls.getCanonicalName() + " must be annotated @Persistable");
+    }
+    
+    // Verify toplevel restrictions
+    if (persistable.toplevel()) {
+      // Verify that the tag is a valid XML tag name - THIS DOES NOT verify that it's unique
+      String tag = persistable.tag();
+      if (tag.isEmpty()) {
+        throw new PersistenceException(cls.getCanonicalName()
+            + " is @Persistable with toplevel=true and so must supply the tag parameter");
+      }
+      if (!isValidXmlTag(tag)) {
+        throw new PersistenceException(cls.getCanonicalName()
+            + "'s @Persistable tag parameter must be a valid XML tag name");
+      }
+      
+      // Verify that the idField is a field name that exists in the class
+      String idField = persistable.idField();
+      if (idField.isEmpty()) {
+        throw new PersistenceException(cls.getCanonicalName()
+            + " is @Persistable with toplevel=true and so must supply the idField parameter");
+      }
+      try {
+        cls.getDeclaredField(idField);
+      } catch (NoSuchFieldException e) {
+        throw new PersistenceException(cls.getCanonicalName()
+            + "'s @Persistable idField parameter must be the name of a present instance-level field");
+      }
+    }
+    
+    return persistable;
+  }
+  
+  /**
+   * Return whether {@code tag} is a valid XML tag.
+   */
+  static boolean isValidXmlTag(String tag) {
+    return tag != null && !tag.isEmpty() && !tag.startsWith("xml") && tag.matches(XML_TAG_REGEX);
+  }
+  
+}
