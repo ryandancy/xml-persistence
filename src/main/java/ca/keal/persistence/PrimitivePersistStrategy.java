a9c0c44bbaeb37346c31e5e0e102e782b08e51f4
Ryan Dancy <ryan@keal.ca>
2019-04-22 21:12:41 -0400

    Add PrimitivePersistStrategy
    It's a very simple PersistenceStrategy which just creates a TextElement
    with the tag being the @Persist annotation's value() and the text being
    the toString() of the object passed in.

---
+++ src/main/java/ca/keal/persistence/PrimitivePersistStrategy.java
@@ -0,0 +1,26 @@
+package ca.keal.persistence;
+
+/**
+ * A {@link PersistenceStrategy} which simply saves the value as a {@link TextElement}'s value using {@code toString()}
+ * on the primitive passed in. This should be used for primitive and string fields.
+ * @param <T> The type of the primitive that will be persisted with this strategy.
+ */
+// TODO do we even need to pass cls in here? It's not used
+class PrimitivePersistStrategy<T> extends PersistenceStrategy<T> {
+  
+  /** Create a new {@link PrimitivePersistStrategy} persisting the specified class. */
+  public PrimitivePersistStrategy(Class<T> cls) {
+    super(cls);
+  }
+  
+  /**
+   * @return A simple {@link TextElement} with the tag of the @{@link Persist} annotation's {@code value} and the text
+   *  being {@code toPersist.toString()}.
+   * @see PersistenceStrategy#persist(ToplevelList, Persist, Object)
+   */
+  @Override
+  public PersistedElement persist(ToplevelList toplevelList, Persist persistAnno, T toPersist) {
+    return new TextElement(persistAnno.value(), toPersist.toString());
+  }
+  
+}
